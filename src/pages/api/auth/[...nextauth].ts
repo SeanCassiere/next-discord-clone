49e969c45945805fa806cac5e2d51b4c1b4dc0e4
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-09-14 10:43:14 +0530

    chore(repo): init project and set-up auth with email links

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      const userUnknown = user as unknown as any;
+      if (session.user) {
+        session.user.id = user.id;
+        session.user.username = userUnknown.username;
+      }
+      return {
+        ...session,
+        ...(session.user ? { user: { ...session.user, username: userUnknown?.username ?? null } } : {}),
+      };
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: env.EMAIL_SERVER_HOST,
+        port: env.EMAIL_SERVER_PORT,
+        auth: {
+          user: env.EMAIL_SERVER_USER,
+          pass: env.EMAIL_SERVER_PASS,
+        },
+      },
+      from: env.EMAIL_FROM,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
