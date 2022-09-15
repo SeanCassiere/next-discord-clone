2bafede3b48bd3de6b22ac1bc99cf6312cd3d5d3
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-09-15 22:08:24 +0530

    style(repo): changes to the discordgray-600 color

--- src/layouts/app/channel-layout/channel-layout.tsx
+++ src/layouts/app/channel-layout/channel-layout.tsx
@@ -6,8 +6,8 @@ const ChannelLayout: React.FC<{
 }> = ({ children, Sidebar }) => {
   return (
     <div className="h-screen w-full flex flex-row">
-      <nav className="flex-0 w-64 bg-discordgray-800">{Sidebar}</nav>
-      <main className="flex-1 w-full bg-discordgray-700">{children}</main>
+      <nav className="flex-0 w-64 bg-discordgray-700">{Sidebar}</nav>
+      <main className="flex-1 w-full bg-discordgray-600">{children}</main>
     </div>
   );
 };
--- src/layouts/app/server-layout/server-layout.tsx
+++ src/layouts/app/server-layout/server-layout.tsx
@@ -3,7 +3,19 @@ import ChannelLayout from "../channel-layout";
 
 const ServerLayout: React.FC<{ serverId: string; channelId?: string }> = ({ serverId, channelId }) => {
   return (
-    <ChannelLayout Sidebar={<div className="h-full">Sidebar</div>}>
+    <ChannelLayout
+      Sidebar={
+        <div className="h-full flex flex-col">
+          <div className="flex-0 bg-red-200 h-28">sidebar header</div>
+          <div className="flex-1 overflow-y-scroll">
+            {Array.from({ length: 100 }).map((_, i) => (
+              <div key={`server-sidebar-${i}`}>sidebar content</div>
+            ))}
+          </div>
+          <div className="flex-0 bg-discordgray-800 h-16">bottom user stuff</div>
+        </div>
+      }
+    >
       <div className="h-full">
         <ul>
           <li>ServerId: {serverId}</li>
